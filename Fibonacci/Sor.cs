6179c3419e8da5810f95bf1b8894afaeeef8b0f8
kovkata <kata.kovacs004@gmail.com>
2024-02-22 15:03:35 +0100

    Add project files.

---
+++ Fibonacci/Sor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    internal class Sor
+
+    {
+        public int Sorszam { get; set; }
+
+        public int Érték {  get; set; } 
+    }
+}
