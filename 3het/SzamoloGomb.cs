ce2d92d32405e9b6655408d16ab40389d16cf277
kovkata <kata.kovacs004@gmail.com>
2024-02-29 17:03:07 +0100

    3.gyakorlat

---
+++ 3het/SzamoloGomb.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3het
+{
+    internal class SzamoloGomb : VillogoGomb
+    {
+        public int szam = 1;
+        public SzamoloGomb()
+        {
+            MouseClick += SzamoloGomb_MouseClick;
+        }
+
+        private void SzamoloGomb_MouseClick(object? sender, MouseEventArgs e)
+        {
+            Text=szam.ToString();
+            szam++;
+            if (szam == 7)
+            {
+                szam = 1;
+            }
+            
+        }
+    }
+}
---
+++ 3het/VillogoGomb.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3het
+{
+    internal class VillogoGomb : Button
+    {
+        public VillogoGomb() 
+        {
+            MouseEnter += VillogoGomb_MouseEnter;
+            MouseLeave += VillogoGomb_MouseLeave;
+        }
+
+        private void VillogoGomb_MouseLeave(object? sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            BackColor = Color.FromArgb(rnd.Next(0, 250), rnd.Next(0, 250), rnd.Next(0, 250));
+        }
+
+        private void VillogoGomb_MouseEnter(object? sender, EventArgs e)
+        {
+            BackColor = Color.Fuchsia;
+        }
+    }
+}
